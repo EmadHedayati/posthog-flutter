1496230ed62785446668a21b881e7ec6d2f28550
emad <hedayati.emad@gmail.com>
2024-07-06 16:41:31 +0330

    bug fix

--- lib/src/posthog_web.dart
+++ lib/src/posthog_web.dart
@@ -17,27 +17,22 @@ class PosthogWeb {
   }
 
   Future<dynamic> handleMethodCall(MethodCall call) async {
-    if (call.method == "init") {
-      print("inside");
+    String tag = "t" + _instances.length.toString();
 
+    if (call.method == "init") {
       final analytics = JsObject.fromBrowserObject(context['posthog']);
-      print(analytics);
 
       dynamic _instance = analytics.callMethod("init", [
         call.arguments["writeKey"],
         {"api_host": call.arguments["posthogHost"]},
-        call.arguments["tag"],
+        tag,
       ]);
-      print(_instance);
 
       _instances.add(_instance);
-      print(_instances.length);
       return;
     }
 
     final instance = _instances[call.arguments["index"]];
-    print("outside");
-    print(instance);
 
     switch (call.method) {
       case 'identify':
